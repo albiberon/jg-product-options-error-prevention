d3a0cdf4ce1a5bdb2c0b69c22d5a8703b6921906
Ates Yanik <albiberon@users.noreply.github.com>
2020-08-13 14:36:40 +0200

    Add files via upload

---
+++ boat-module-error-prevention.js
@@ -0,0 +1,42 @@
+require(["jquery", "domReady!"], function ($) {
+
+  // jQuery(function($){
+
+//   $('custom-options.field').each(function(){
+
+//     if($(this).find('label'))
+
+//   });
+
+//   $("#select_11684").on("change", function () {
+//     if ($(this).find("option:selected").text().includes("Boat")) {
+//       $('#select_11685 option:contains("Picnic")').attr("disabled", "true");
+//       $('#select_11685 option:not(:contains("Picnic"))').removeAttr("disabled");
+//       $('#select_11685 option:contains("Boat")').attr("disabled", "true");
+//     } else if ($(this).find("option:selected").text().includes("Picnic")) {
+//       $('#select_11685 option:contains("Boat")').attr("disabled", "true");
+//       $('#select_11685 option:not(:contains("Boat"))').removeAttr("disabled");
+//       $('#select_11685 option:contains("Picnic")').attr("disabled", "true");
+//     } else {
+//       $("#select_11685 option").each(function () {
+//         $(this).removeAttr("disabled");
+//       });
+//     }
+//   });
+
+//   $("#select_11685").on("change", function () {
+//     if ($(this).find("option:selected").text().includes("Boat")) {
+//       $('#select_11684 option:contains("Picnic")').attr("disabled", "true");
+//       $('#select_11684 option:not(:contains("Picnic"))').removeAttr("disabled");
+//       $('#select_11684 option:contains("Boat")').attr("disabled", "true");
+//     } else if ($(this).find("option:selected").text().includes("Picnic")) {
+//       $('#select_11684 option:contains("Boat")').attr("disabled", "true");
+//       $('#select_11684 option:not(:contains("Boat"))').removeAttr("disabled");
+//       $('#select_11684 option:contains("Picnic")').attr("disabled", "true");
+//     } else {
+//       $("#select_11684 option").each(function () {
+//         $(this).removeAttr("disabled");
+//       });
+//     }
+//   });
+});
